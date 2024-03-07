8a4fd43543f5d1474812362831cc802fb8896cea
JoyTsing <792566033@qq.com>
2024-03-07 18:11:24 +0800

    [fix]

--- format_count.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 格式化
-clang-format-17 -i $(find include/ -type f -name "*.h")
-clang-format-17 -i $(find src/ -type f -name "*.cpp")
-
-# 统计代码行数
-cloc --git `git branch --show-current`
