dec68b4964ad40a7e1f891067b58710044d93fd5
JoyTsing <792566033@qq.com>
2024-03-07 18:17:03 +0800

    [format]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include "thread/threadpool.h"
 #include "minilog/minilog.h"
+#include "thread/threadpool.h"
+
 int main() {
-    minilog::log_info("hello world : {}", 42);
-    return 0;
+  minilog::log_info("hello world : {}", 42);
+  return 0;
 }
